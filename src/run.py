ad97e859f2428029f8eb586408bcef35a271c8af
wasabina67 <wasabina67@gmail.com>
2024-07-26 13:42:01 +0000

    Update run.py

--- src/run.py
+++ src/run.py
@@ -1,9 +1,22 @@
+import os
+
+from data import yt_list
 from utils import validate_yt_list
 
 
 def main():
     if not validate_yt_list():
         raise RuntimeError("Validation failed.")
+
+    for item in yt_list:
+        videoid = item["id"]
+        output_videoid = f"output/{videoid}"
+        mp4_file_path = output_videoid + ".mp4"
+
+        if os.path.isfile(mp4_file_path):
+            continue
+
+        # check_video_deletion()
 
 
 if __name__ == "__main__":
