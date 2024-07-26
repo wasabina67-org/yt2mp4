c7171705e09e145b1b8769debf8a3b57519a9091
wasabina67 <wasabina67@gmail.com>
2024-07-26 22:29:21 +0900

    Create data.py.example, run.py, utils.py

---
+++ src/utils.py
@@ -0,0 +1,39 @@
+from data import yt_list
+
+
+def validate_yt_list():
+    seen_ids = set()
+    seen_title_artist = set()
+
+    for item in yt_list:
+        videoid = item.get("id")
+        if not videoid:
+            # None or Empty
+            return False
+        if videoid in seen_ids:
+            # Duplicate
+            return False
+        seen_ids.add(videoid)
+
+        metadata = item.get("metadata")
+        if not metadata:
+            # None or Empty
+            return False
+
+        title = metadata.get("title")
+        if not title:
+            # None or Empty
+            return False
+
+        artist = metadata.get("artist")
+        if not artist:
+            # None or Empty
+            return False
+
+        title_artist = title + artist
+        if title_artist in seen_title_artist:
+            # Duplicate
+            return False
+        seen_title_artist.add(title_artist)
+
+        return True
