b58aad7f23ace65d1e0f88174fdcc1c4d76d5221
wasabina67 <wasabina67@gmail.com>
2024-08-12 01:21:05 +0000

    Update run.py

--- src/run.py
+++ src/run.py
@@ -36,7 +36,6 @@ def main():
 
         ydl_opts["outtmpl"] = output_videoid
         with YoutubeDL(ydl_opts) as ydl:
-            url = f"https://www.youtube.com/watch?v={videoid}"
             retcode = ydl.download([url])
             if retcode != 0:
                 raise RuntimeError(
